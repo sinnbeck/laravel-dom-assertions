ab7331caff79f624c5a831d269c768c3798bfd9e
Muhammed Sarı <24608797+mabdullahsari@users.noreply.github.com>
2022-10-24 22:16:45 +0200

    Refactor to mixin

--- src/DomAssertionsServiceProvider.php
+++ src/DomAssertionsServiceProvider.php
@@ -4,14 +4,13 @@ namespace Sinnbeck\DomAssertions;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Testing\TestResponse;
-use Sinnbeck\DomAssertions\Macros\AssertElementMacro;
-use Sinnbeck\DomAssertions\Macros\AssertFormMacro;
 
 class DomAssertionsServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function boot()
     {
-        TestResponse::macro('assertForm', app()->call(AssertFormMacro::class));
-        TestResponse::macro('assertElement', app()->call(AssertElementMacro::class));
+        if ($this->app->runningUnitTests()) {
+            TestResponse::mixin(new TestResponseMacros());
+        }
     }
 }
--- src/TestResponseMacros.php
+++ src/TestResponseMacros.php
@@ -2,17 +2,58 @@
 
 declare(strict_types=1);
 
-namespace Sinnbeck\DomAssertions\Macros;
+namespace Sinnbeck\DomAssertions;
 
+use Closure;
 use DOMException;
 use Illuminate\Testing\TestResponse;
 use PHPUnit\Framework\Assert;
+use Sinnbeck\DomAssertions\Asserts\ElementAssert;
 use Sinnbeck\DomAssertions\Asserts\FormAssert;
 use Sinnbeck\DomAssertions\Support\DomParser;
 
-class AssertFormMacro
+/**
+ * @mixin TestResponse
+ */
+class TestResponseMacros
 {
-    public function __invoke(): \Closure
+    public function assertElement(): Closure
+    {
+        return function ($selector = 'body', $callback = null): TestResponse {
+            /** @var TestResponse $this */
+            Assert::assertNotEmpty(
+                $this->getContent(),
+                'The view is empty!'
+            );
+
+            try {
+                $parser = DomParser::new($this->getContent());
+            } catch (DOMException $exception) {
+                Assert::fail($exception->getMessage());
+            }
+
+            if (is_callable($selector)) {
+                $callback = $selector;
+                $selector = 'body';
+            }
+
+            if (is_string($selector)) {
+                $element = $parser->query($selector);
+            } else {
+                Assert::fail('Invalid selector!');
+            }
+
+            Assert::assertNotNull($element, sprintf('No element found with selector: %s', $selector));
+
+            if ($callback) {
+                $callback(new ElementAssert($this->getContent(), $element));
+            }
+
+            return $this;
+        };
+    }
+
+    public function assertForm(): Closure
     {
         return function ($selector = 'form', $callback = null): TestResponse {
             /** @var TestResponse $this */
