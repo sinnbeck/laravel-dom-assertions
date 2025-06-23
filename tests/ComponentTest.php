439752cc2d89d62af0a0babe59e8f0149faf72a7
Jamie <ziadoz@gmail.com>
2025-06-23 15:21:02 +0100

    wip (html5, livewire)

--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -6,6 +6,7 @@ use Tests\Views\Components\BrokenComponent;
 use Tests\Views\Components\EmptyComponent;
 use Tests\Views\Components\EmptyElementComponent;
 use Tests\Views\Components\Html5Component;
+use Tests\Views\Components\LivewireComponent;
 use Tests\Views\Components\NestedComponent;
 
 it('can handle an empty component', function () {
@@ -29,11 +30,6 @@ it('can parse broken html', function () {
         ->assertElementExists();
 });
 
-it('can find a nested element', function () {
-    $this->component(NestedComponent::class)
-        ->assertElementExists('section');
-});
-
 it('can find the section', function () {
     $this->component(NestedComponent::class)
         ->assertElementExists('section', function (AssertElement $assert) {
@@ -53,3 +49,312 @@ it('can fail checking for html5', function () {
     AssertionFailedError::class,
     'Not a html5 doctype!'
 );
+
+it('can fail finding a class', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('body', function (AssertElement $assert) {
+            $assert->find('#nav', function (AssertElement $element) {
+                $element->doesntHave('class');
+            });
+        });
+});
+
+it('can fail finding a href with exact match', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('body', function (AssertElement $assert) {
+            $assert->find('#nav a', function (AssertElement $element) {
+                $element->has('href')
+                    ->doesntHave('href', '/bar');
+            });
+        });
+});
+
+it('can fail when finding a id that isnt expected', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('body', function (AssertElement $assert) {
+            $assert->find('#nav', function (AssertElement $element) {
+                $element->doesntHave('id');
+            });
+        });
+})->throws(
+    AssertionFailedError::class,
+    'Found an attribute "id"'
+);
+
+it('can fail when finding a href with matching value that isnt expected', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('body', function (AssertElement $assert) {
+            $assert->find('#nav a', function (AssertElement $element) {
+                $element->doesntHave('href', '/foo');
+            });
+        });
+})->throws(
+    AssertionFailedError::class,
+    'Found an attribute "href" with value "/foo"'
+);
+
+it('can find an element by selector', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('#nav');
+});
+
+it('can fail finding anything', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('div > nav');
+})->throws(
+    AssertionFailedError::class,
+    'No element found with selector: div > nav'
+);
+
+it('can check the element has the correct type', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('#nav', function (AssertElement $element) {
+            $element->is('nav');
+        });
+});
+
+it('can fail matching element type', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('#nav', function (AssertElement $element) {
+            $element->is('div');
+        });
+})->throws(
+    AssertionFailedError::class,
+    'Element is not of type "div"'
+);
+
+it('can fail with wrong type of selector', function () {
+    $this->view('form')
+        ->assertElementExists(['div']);
+})->throws(AssertionFailedError::class, 'Invalid selector!');
+
+it('can find a nested element', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->containsDiv();
+        });
+});
+
+it('can find a nested element with content', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('div', [
+                'class' => 'foobar',
+            ]);
+        });
+});
+
+it('can match text content', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('span.bar', function (AssertElement $element) {
+            $element->has('text', 'Foo');
+        });
+});
+
+it('can match text content with duplicate spaces and vertical whitespace', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('p.foo.bar', function (AssertElement $element) {
+            $element->has('text', 'Foo Bar');
+        });
+});
+
+it('can match text content containing a string', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('p.foo.bar', function (AssertElement $element) {
+            $element->containsText('Bar');
+        });
+});
+
+it('can match text content containing a string ignoring case', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('p.foo.bar', function (AssertElement $element) {
+            $element->containsText('bar', true);
+        });
+});
+
+it('can match text content not containing a string', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists('p.foo.bar', function (AssertElement $element) {
+            $element->doesntContainText('bar');
+        });
+});
+
+it('can match a class no matter the order', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('span', [
+                'class' => 'foo bar',
+            ]);
+            $element->find('span', function (AssertElement $span) {
+                $span->has('class', 'foo bar');
+            });
+        });
+});
+
+it('can match a partial class', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('span', [
+                'class' => 'foo bar',
+            ]);
+            $element->find('span', function (AssertElement $span) {
+                $span->has('class', 'bar');
+            });
+        });
+});
+
+it('can find multiple identical items', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('div', [], 4);
+        });
+});
+
+it('can find multiple identical items simplified', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('div', 4);
+        });
+});
+
+it('can find multiple identical items with content', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('ul > li', [
+                'x-data' => 'foobar',
+            ], 2);
+        });
+});
+
+it('can find multiple identical items with content ensuring no wrong matches', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('div', [
+                'x-data' => 'foobar',
+            ], 1);
+        });
+});
+
+it('can fail finding a nested element with content', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('div', [
+                'class' => 'foo',
+            ]);
+        });
+})->throws(AssertionFailedError::class, 'Could not find a matching "div" with data:');
+
+it('can find a nested element with content functional', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->findDiv(function (AssertElement $element) {
+                $element->is('div');
+            });
+        });
+});
+
+it('can find a nested element multiple levels', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->findDiv(function (AssertElement $element) {
+                $element->is('div');
+                $element->find('div', function (AssertElement $element) {
+                    $element->is('div');
+                    $element->findDiv(function (AssertElement $element) {
+                        $element->is('div');
+                    });
+                });
+            });
+        });
+});
+
+it('can find a nested element multiple levels by query', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->findDiv(function (AssertElement $element) {
+                $element->is('div');
+                $element->find('.deep', function (AssertElement $element) {
+                    $element->is('div');
+                    $element->findSpan(function (AssertElement $element) {
+                        $element->is('span');
+                    });
+                });
+            });
+        });
+});
+
+it('can find a nested element multiple levels by query and attributes', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->findDiv(function (AssertElement $element) {
+                $element->is('div');
+                $element->contains('.deep', [
+                    'class' => 'deep',
+                ]);
+            });
+        });
+});
+
+it('can find a nested element and ensure doesnt contain', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->findDiv(function (AssertElement $element) {
+                $element->is('div');
+                $element->doesntContain('nav');
+            });
+        });
+});
+
+it('can fail finding an contained element', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->findDiv(function (AssertElement $element) {
+                $element->doesntContain('div');
+            });
+        });
+})->throws(
+    AssertionFailedError::class,
+    'Found a matching element of type "div'
+);
+
+it('can fail finding an contained element with query', function () {
+    $this->component(Html5Component::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->findDiv(function (AssertElement $element) {
+                $element->doesntContain('div.foobar');
+            });
+        });
+})->throws(
+    AssertionFailedError::class,
+    'Found a matching element of type "div'
+);
+
+it('can match on livewire attributes', function () {
+    $this->component(LivewireComponent::class)
+        ->assertElementExists('[wire\:model="foo"]', function (AssertElement $element) {
+            $element->is('input');
+        });
+});
+
+it('can match has on livewire attributes', function () {
+    $this->component(LivewireComponent::class)
+        ->assertElementExists('input', function (AssertElement $element) {
+            $element->has('wire:model', 'foo');
+        });
+});
+
+it('can match on livewire with contains', function () {
+    $this->component(LivewireComponent::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('input[wire\:model="foo"]');
+        });
+});
+
+it('can match on livewire contains as attribute', function () {
+    $this->component(LivewireComponent::class)
+        ->assertElementExists(function (AssertElement $element) {
+            $element->contains('input', [
+                'wire:model' => 'foo',
+            ]);
+        });
+});
