9dadba5f332e7f304d558adf5169cfd95faddf3b
Jamie <ziadoz@gmail.com>
2025-06-23 13:15:59 +0100

    wip (pint)

--- tests/views/components/EmptyComponent.php
+++ tests/views/components/EmptyComponent.php
@@ -6,9 +6,7 @@ use Illuminate\View\Component;
 
 class EmptyComponent extends Component
 {
-    public function __construct()
-    {
-    }
+    public function __construct() {}
 
     public function render(): string
     {
