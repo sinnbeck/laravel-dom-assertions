7ec3b1939f41a2085852021e1aab770dbe7dcf65
Jamie <ziadoz@gmail.com>
2025-06-23 15:51:14 +0100

    wip (before skip)

--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Traits\Macroable;
 use Illuminate\Testing\TestComponent;
 use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\TestCase;
 use Sinnbeck\DomAssertions\Asserts\AssertElement;
 use Tests\Views\Components\BrokenComponent;
 use Tests\Views\Components\EmptyBodyComponent;
@@ -11,8 +12,11 @@ use Tests\Views\Components\Html5Component;
 use Tests\Views\Components\LivewireComponent;
 use Tests\Views\Components\NestedComponent;
 
-$testComponentMacroable = in_array(Macroable::class, class_uses(TestComponent::class) ?? []);
-$testComponentMacroableMsg = 'Testing Blade components is unavailable in this version of Laravel';
+beforeEach(function () {
+    if (! in_array(Macroable::class, class_uses(TestComponent::class) ?? [])) {
+        TestCase::markTestSkipped('Testing Blade components is unavailable in this version of Laravel');
+    }
+});
 
 it('can handle an empty component', function () {
     $this->component(EmptyComponent::class)
@@ -20,7 +24,7 @@ it('can handle an empty component', function () {
 })->throws(
     AssertionFailedError::class,
     'The component is empty!'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can handle an empty body', function () {
     $this->component(EmptyBodyComponent::class)
@@ -28,29 +32,29 @@ it('can handle an empty body', function () {
 })->throws(
     AssertionFailedError::class,
     'No element found with selector: body'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can parse broken html', function () {
     $this->component(BrokenComponent::class)
         ->assertElementExists();
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find the element', function () {
     $this->component(NestedComponent::class)
         ->assertElementExists();
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find the body', function () {
     $this->component(NestedComponent::class)
         ->assertElementExists('body', function (AssertElement $assert) {
             $assert->is('body');
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can check for html5', function () {
     $this->component(Html5Component::class)
         ->assertHtml5();
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can fail checking for html5', function () {
     $this->component(NestedComponent::class)
@@ -58,7 +62,7 @@ it('can fail checking for html5', function () {
 })->throws(
     AssertionFailedError::class,
     'Not a html5 doctype!'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can fail finding a class', function () {
     $this->component(Html5Component::class)
@@ -67,7 +71,7 @@ it('can fail finding a class', function () {
                 $element->doesntHave('class');
             });
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can fail finding a href with exact match', function () {
     $this->component(Html5Component::class)
@@ -77,7 +81,7 @@ it('can fail finding a href with exact match', function () {
                     ->doesntHave('href', '/bar');
             });
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can fail when finding a id that isnt expected', function () {
     $this->component(Html5Component::class)
@@ -89,7 +93,7 @@ it('can fail when finding a id that isnt expected', function () {
 })->throws(
     AssertionFailedError::class,
     'Found an attribute "id"'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can fail when finding a href with matching value that isnt expected', function () {
     $this->component(Html5Component::class)
@@ -101,12 +105,12 @@ it('can fail when finding a href with matching value that isnt expected', functi
 })->throws(
     AssertionFailedError::class,
     'Found an attribute "href" with value "/foo"'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can find an element by selector', function () {
     $this->component(Html5Component::class)
         ->assertElementExists('#nav');
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can fail finding anything', function () {
     $this->component(Html5Component::class)
@@ -114,14 +118,14 @@ it('can fail finding anything', function () {
 })->throws(
     AssertionFailedError::class,
     'No element found with selector: div > nav'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can check the element has the correct type', function () {
     $this->component(Html5Component::class)
         ->assertElementExists('#nav', function (AssertElement $element) {
             $element->is('nav');
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can fail matching element type', function () {
     $this->component(Html5Component::class)
@@ -131,7 +135,7 @@ it('can fail matching element type', function () {
 })->throws(
     AssertionFailedError::class,
     'Element is not of type "div"'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can fail with wrong type of selector', function () {
     $this->view('form')
@@ -139,14 +143,14 @@ it('can fail with wrong type of selector', function () {
 })->throws(
     AssertionFailedError::class,
     'Invalid selector!'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can find a nested element', function () {
     $this->component(Html5Component::class)
         ->assertElementExists(function (AssertElement $element) {
             $element->containsDiv();
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find a nested element with content', function () {
     $this->component(Html5Component::class)
@@ -155,42 +159,42 @@ it('can find a nested element with content', function () {
                 'class' => 'foobar',
             ]);
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match text content', function () {
     $this->component(Html5Component::class)
         ->assertElementExists('span.bar', function (AssertElement $element) {
             $element->has('text', 'Foo');
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match text content with duplicate spaces and vertical whitespace', function () {
     $this->component(Html5Component::class)
         ->assertElementExists('p.foo.bar', function (AssertElement $element) {
             $element->has('text', 'Foo Bar');
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match text content containing a string', function () {
     $this->component(Html5Component::class)
         ->assertElementExists('p.foo.bar', function (AssertElement $element) {
             $element->containsText('Bar');
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match text content containing a string ignoring case', function () {
     $this->component(Html5Component::class)
         ->assertElementExists('p.foo.bar', function (AssertElement $element) {
             $element->containsText('bar', true);
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match text content not containing a string', function () {
     $this->component(Html5Component::class)
         ->assertElementExists('p.foo.bar', function (AssertElement $element) {
             $element->doesntContainText('bar');
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match a class no matter the order', function () {
     $this->component(Html5Component::class)
@@ -202,7 +206,7 @@ it('can match a class no matter the order', function () {
                 $span->has('class', 'foo bar');
             });
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match a partial class', function () {
     $this->component(Html5Component::class)
@@ -214,21 +218,21 @@ it('can match a partial class', function () {
                 $span->has('class', 'bar');
             });
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find multiple identical items', function () {
     $this->component(Html5Component::class)
         ->assertElementExists(function (AssertElement $element) {
             $element->contains('div', [], 4);
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find multiple identical items simplified', function () {
     $this->component(Html5Component::class)
         ->assertElementExists(function (AssertElement $element) {
             $element->contains('div', 4);
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find multiple identical items with content', function () {
     $this->component(Html5Component::class)
@@ -237,7 +241,7 @@ it('can find multiple identical items with content', function () {
                 'x-data' => 'foobar',
             ], 2);
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find multiple identical items with content ensuring no wrong matches', function () {
     $this->component(Html5Component::class)
@@ -246,7 +250,7 @@ it('can find multiple identical items with content ensuring no wrong matches', f
                 'x-data' => 'foobar',
             ], 1);
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can fail finding a nested element with content', function () {
     $this->component(Html5Component::class)
@@ -258,7 +262,7 @@ it('can fail finding a nested element with content', function () {
 })->throws(
     AssertionFailedError::class,
     'Could not find a matching "div" with data:'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can find a nested element with content functional', function () {
     $this->component(Html5Component::class)
@@ -267,7 +271,7 @@ it('can find a nested element with content functional', function () {
                 $element->is('div');
             });
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find a nested element multiple levels', function () {
     $this->component(Html5Component::class)
@@ -282,7 +286,7 @@ it('can find a nested element multiple levels', function () {
                 });
             });
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find a nested element multiple levels by query', function () {
     $this->component(Html5Component::class)
@@ -297,7 +301,7 @@ it('can find a nested element multiple levels by query', function () {
                 });
             });
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find a nested element multiple levels by query and attributes', function () {
     $this->component(Html5Component::class)
@@ -309,7 +313,7 @@ it('can find a nested element multiple levels by query and attributes', function
                 ]);
             });
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can find a nested element and ensure doesnt contain', function () {
     $this->component(Html5Component::class)
@@ -319,7 +323,7 @@ it('can find a nested element and ensure doesnt contain', function () {
                 $element->doesntContain('nav');
             });
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can fail finding an contained element', function () {
     $this->component(Html5Component::class)
@@ -331,7 +335,7 @@ it('can fail finding an contained element', function () {
 })->throws(
     AssertionFailedError::class,
     'Found a matching element of type "div'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can fail finding an contained element with query', function () {
     $this->component(Html5Component::class)
@@ -343,28 +347,28 @@ it('can fail finding an contained element with query', function () {
 })->throws(
     AssertionFailedError::class,
     'Found a matching element of type "div'
-)->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+);
 
 it('can match on livewire attributes', function () {
     $this->component(LivewireComponent::class)
         ->assertElementExists('[wire\:model="foo"]', function (AssertElement $element) {
             $element->is('input');
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match has on livewire attributes', function () {
     $this->component(LivewireComponent::class)
         ->assertElementExists('input', function (AssertElement $element) {
             $element->has('wire:model', 'foo');
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match on livewire with contains', function () {
     $this->component(LivewireComponent::class)
         ->assertElementExists(function (AssertElement $element) {
             $element->contains('input[wire\:model="foo"]');
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
 
 it('can match on livewire contains as attribute', function () {
     $this->component(LivewireComponent::class)
@@ -373,4 +377,4 @@ it('can match on livewire contains as attribute', function () {
                 'wire:model' => 'foo',
             ]);
         });
-})->skip(! $testComponentMacroable, $testComponentMacroableMsg);
+});
