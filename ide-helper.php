d69683ba9b3eff91393f63cc594b542371575c82
Jack Bayliss <jack@fusionspim.com>
2025-10-09 12:13:27 +0100

    Update ide-helper.php (#36)

--- ide-helper.php
+++ ide-helper.php
@@ -44,37 +44,37 @@ namespace Illuminate\Testing {
     {
         public function assertHtml5()
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestView $instance */
             return $instance;
         }
 
         public function assertElement($selector = 'body', $callback = null)
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestView $instance */
             return $instance;
         }
 
         public function assertElementExists($selector = 'body', $callback = null)
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestView $instance */
             return $instance;
         }
 
         public function assertForm($selector = 'form', $callback = null)
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestView $instance */
             return $instance;
         }
 
         public function assertFormExists($selector = 'form', $callback = null)
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestView $instance */
             return $instance;
         }
 
         public function assertContainsElement($selector, array $attributes = [])
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestView $instance */
             return $instance;
         }
     }
@@ -83,31 +83,31 @@ namespace Illuminate\Testing {
     {
         public function assertHtml5()
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestComponent $instance */
             return $instance;
         }
 
         public function assertElement($selector = 'body', $callback = null)
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestComponent $instance */
             return $instance;
         }
 
         public function assertElementExists($selector = 'body', $callback = null)
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestComponent $instance */
             return $instance;
         }
 
         public function assertForm($selector = 'form', $callback = null)
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestComponent $instance */
             return $instance;
         }
 
         public function assertFormExists($selector = 'form', $callback = null)
         {
-            /** @var \Illuminate\Testing\TestResponse $instance */
+            /** @var \Illuminate\Testing\TestComponent $instance */
             return $instance;
         }
 
