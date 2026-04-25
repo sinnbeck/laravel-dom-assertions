08c40b825a0d1e103e62daffb5b601ce44cd1f0d
Jack <jjbayliss@icloud.com>
2026-04-26 00:45:10 +0100

    tests

--- src/TestComponentMacros.php
+++ src/TestComponentMacros.php
@@ -137,7 +137,7 @@ class TestComponentMacros
 
                     if ($attribute === 'text') {
                         $needle = (string) $expected;
-                        $rawText = $element->textContent;
+                        $rawText = str_replace("\r\n", "\n", $element->textContent);
                         $normalizedText = Normalize::text($rawText);
 
                         if (str_contains($normalizedText, $needle) || str_contains($rawText, $needle)) {
